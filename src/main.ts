18ac99353d78ab1af825981acdff4173fde42b82
Wagner Xu <wagnerhsu@hotmail.com>
2022-11-23 22:05:12 +0800

    add vconsole

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VConsole from 'vconsole'
+
+const vConsole = new VConsole()
+console.log('vConsole enabled')
 
 createApp(App).mount('#app')
